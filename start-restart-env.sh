84d5230e628bb6bf28cfc2f75ee84f09ec258ea3
CMYanko <cmyanko@gmail.com>
2017-12-01 10:56:57 -0500

    Switching to official image
    updated scripts to use official image

--- start-restart-env.sh
+++ start-restart-env.sh
@@ -1,9 +1,11 @@
+# Use this file if you're not using docker-compose
+
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 docker ps
 docker network create demo-net
 docker pull bradbeck/nexus-https
-docker pull cmyanko/demo-iq-server
+docker pull sonatype/nexus-iq-server
 docker run -d -p 8070:8070 -v ~/iq-data:/sonatype-work --net demo-net --name iq-server cmyanko/demo-iq-server
 docker run -d -p 8081:8081 -p 8443:8443 -p18443:18443 -p18444:18444 --net demo-net -v ~/nexus-data:/nexus-data -v ~/nexus-ssl:/opt/sonatype/nexus/etc/ssl --name nexus3 bradbeck/nexus-https
 docker ps
