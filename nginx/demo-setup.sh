5e1afda440218dcc4a8817a22354a496062101b1
CMYanko <cmyanko@gmail.com>
2018-01-04 18:18:42 -0500

    group and proxy

--- nginx/demo-setup.sh
+++ nginx/demo-setup.sh
@@ -11,4 +11,11 @@ until curl --fail --insecure http://localhost:8081; do
 done
 
 #This needs to be changed but is left here as an example
-./nexus-repository/provision.sh
+curl -v -u admin:admin123 --insecure --header 'Content-Type: application/json' 'http://localhost/service/siesta/rest/v1/script' -d @nexus-repository/create-docker-proxy.json
+curl -v -X POST -u admin:admin123 --insecure --header 'Content-Type: text/plain' 'http://localhost/service/siesta/rest/v1/script/CreateDockerProxy/run'
+
+curl -v -u admin:admin123 --insecure --header 'Content-Type: application/json' 'http://localhost/service/siesta/rest/v1/script' -d @nexus-repository/create-docker-hosted.json
+curl -v -X POST -u admin:admin123 --insecure --header 'Content-Type: text/plain' 'http://localhost/service/siesta/rest/v1/script/CreateDockerHosted/run'
+
+curl -v -u admin:admin123 --insecure --header 'Content-Type: application/json' 'http://localhost/service/siesta/rest/v1/script' -d @nexus-repository/create-docker-group.json
+curl -v -X POST -u admin:admin123 --insecure --header 'Content-Type: text/plain' 'http://localhost/service/siesta/rest/v1/script/CreateDockerGroup/run'
