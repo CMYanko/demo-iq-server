ae1ce51ea652498999389ae2a2fac179a2372ac6
CMYanko <cmyanko@gmail.com>
2018-01-05 09:10:14 -0500

    added scripts from book examples

--- nginx/demo-setup.sh
+++ nginx/demo-setup.sh
@@ -11,11 +11,8 @@ until curl --fail --insecure http://localhost:8081; do
 done
 
 #Create Docker repos and group
-curl -v -u admin:admin123 --insecure --header 'Content-Type: application/json' 'https://localhost/service/siesta/rest/v1/script' -d @nexus-repository/create-docker-proxy.json
-curl -v -X POST -u admin:admin123 --insecure --header 'Content-Type: text/plain' 'https://localhost/service/siesta/rest/v1/script/CreateDockerProxy/run'
-
-curl -v -u admin:admin123 --insecure --header 'Content-Type: application/json' 'https://localhost/service/siesta/rest/v1/script' -d @nexus-repository/create-docker-hosted.json
-curl -v -X POST -u admin:admin123 --insecure --header 'Content-Type: text/plain' 'https://localhost/service/siesta/rest/v1/script/CreateDockerHosted/run'
-
-curl -v -u admin:admin123 --insecure --header 'Content-Type: application/json' 'https://localhost/service/siesta/rest/v1/script' -d @nexus-repository/create-docker-group.json
-curl -v -X POST -u admin:admin123 --insecure --header 'Content-Type: text/plain' 'https://localhost/service/siesta/rest/v1/script/CreateDockerGroup/run'
+#curl -v -u admin:admin123 --insecure --header 'Content-Type: application/json' 'https://localhost/service/siesta/rest/v1/script' -d @nexus-repository/docker.json
+#curl -v -X POST -u admin:admin123 --insecure --header 'Content-Type: text/plain' 'https://localhost/service/siesta/rest/v1/script/Docker/run'
+cd nexus-repository
+./create.sh docker.json
+./run.sh Docker
