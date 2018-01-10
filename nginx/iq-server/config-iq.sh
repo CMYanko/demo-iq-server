07c022f1f4e18ba6739d968438fcbe6d748a8f63
CMYanko <cmyanko@gmail.com>
2018-01-10 12:14:42 -0500

    fixing directory movement and added comments

--- nginx/iq-server/config-iq.sh
+++ nginx/iq-server/config-iq.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
+#Note: defaults creds are baked in so this will work on a fresh instance
 nexus() {
 java -jar nexus-cli-1.0-SNAPSHOT-shaded.jar \
   -s http://localhost:8070 \
   $@
 }
 
-#cd $(cd ${0%/*}; pwd)
+cd $(cd ${0%/*}; pwd)
 
 echo "Importing license"
-#Update this to point to your license
+#Update this to point to your license placed in this folder
 nexus license install sonatype-nexus-firewall-lifecycle-2017.lic
 
-
+#these are our sample policies but you might have customized and exported your own so feel free to update
 echo "Applying policies"
 nexus policy import Sonatype-Sample-Policy-Set-1.22.json
+
+cd ..
+pwd
