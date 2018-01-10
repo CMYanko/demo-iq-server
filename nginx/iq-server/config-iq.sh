8fff76c1b23dea04673f53ebb1f28fc80121dd6f
CMYanko <cmyanko@gmail.com>
2018-01-10 12:17:33 -0500

    more comments

--- nginx/iq-server/config-iq.sh
+++ nginx/iq-server/config-iq.sh
@@ -10,7 +10,8 @@ java -jar nexus-cli-1.0-SNAPSHOT-shaded.jar \
 cd $(cd ${0%/*}; pwd)
 
 echo "Importing license"
-#Update this to point to your license placed in this folder
+# Update this to point to your license placed in this folder
+#nexus license install <your license file>
 nexus license install sonatype-nexus-firewall-lifecycle-2017.lic
 
 #these are our sample policies but you might have customized and exported your own so feel free to update
